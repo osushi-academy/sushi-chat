d0ce551aa61bf83ecad5cf42a5c45e971b780a87
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-04-20 01:21:17 +0900

    SocketIOのテストを追加

--- server/src/__test__/webSocket.ts
+++ server/src/__test__/webSocket.ts
@@ -1,41 +1,50 @@
 import { createServer, Server as HttpServer } from 'http'
-import { Server, Socket as ServerSocket } from 'socket.io'
+import { Server } from 'socket.io'
 import { io as Client, Socket as ClientSocket } from 'socket.io-client'
+import { ArrayRange } from '../utils/range'
 
+// TODO: 本来はサーバーサイドの本実装におきかわる部分
 const createCustomServer = (httpServer: HttpServer) => {
   const io = new Server(httpServer)
   io.on('connection', (socket) => {
-    socket.on("hello", ({ username }) => {
-      socket.emit('hello-return', `hello, ${username}!!`)
+    socket.on("HELLO", ({ username }) => {
+      socket.emit('HELLO_REPLY', `hello, ${username}!!`)
     })
   })
   return io
 }
 
 describe('WebSocketのテスト', () => {
-  let io: Server
-  let clientSocket: ClientSocket
+  const CLIENTS_COUNT = 3
 
+  let io: Server
+  let clientSockets: ClientSocket[]
+
+  // WebSocketのセットアップ
   beforeAll((done) => {
     const httpServer = createServer()
     io = createCustomServer(httpServer)
-    httpServer.listen(() => {
+    httpServer.listen(async () => {
       const port = (httpServer as any).address().port
-      clientSocket = Client(`http://localhost:${port}`)
-      clientSocket.on("connect", done)
+      clientSockets = ArrayRange(CLIENTS_COUNT).map(() => Client(`http://localhost:${port}`))
+      await Promise.all(clientSockets.map(socket =>
+            new Promise((resolve) => socket.on("connect", () => resolve('FIN')))))
+      done()
     })
   })
 
+  // WebSocketのクローズ
   afterAll(() => {
     io.close()
-    clientSocket.close()
+    clientSockets.forEach(socket => socket.close())
   })
 
+  // おうむ返しテスト
   test("Hello test", (done) => {
-    clientSocket.on("hello-return", (arg: string) => {
+    clientSockets[0].on("HELLO_REPLY", (arg: string) => {
       expect(arg).toBe("hello, Alice!!")
       done()
     })
-    clientSocket.emit("hello", { username: "Alice" })
+    clientSockets[0].emit("HELLO", { username: "Alice" })
   });
 })
