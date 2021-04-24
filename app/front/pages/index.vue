7f89143174956336f00d17df7c3e919e8c57537b
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-04-24 15:49:20 +0900

    add sample modal

--- app/front/pages/index.vue
+++ app/front/pages/index.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="container page">
+    <modal name="hello-world">
+      <div class="modal-header">
+        <h2>トピック作成</h2>
+      </div>
+      <div class="modal-body">
+        <div v-for="(topic, index) in topics" :key="index">
+          <input
+            v-model="topics[index]"
+            class="textarea"
+            contenteditable
+            placeholder="トピック名"
+            @keydown.enter.exact="addTopic"
+          />
+          <button type="button" @click="removeTopic(index)">削除</button>
+        </div>
+        <button type="button" @click="addTopic">追加</button>
+        <button type="button" @click="hide">はじめる</button>
+      </div>
+    </modal>
     <div v-for="topic in topics" :key="topic.id">
       <ChatRoom :topic="topic" />
     </div>
@@ -8,6 +27,9 @@
 
 <script lang="ts">
 import Vue from 'vue'
+// @ts-ignore
+import { v4 as uuidv4 } from 'uuid'
+import VModal from 'vue-js-modal'
 import * as Model from '@/models/contents'
 import ChatRoom from '@/components/ChatRoom.vue'
 
@@ -16,7 +38,7 @@ type DataType = {
   topics: Model.Topic[]
   isNotify: boolean
 }
-
+Vue.use(VModal)
 export default Vue.extend({
   name: 'Index',
   components: {
@@ -38,6 +60,24 @@ export default Vue.extend({
       ],
       isNotify: false,
     }
+  },
+  mounted(): any {
+    this.$modal.show('select-modal')
+  },
+  methods: {
+    // modalを消し、topic作成
+    hide(): any {
+      this.topics.push()
+      this.$modal.hide('select-modal')
+    },
+    // 該当するtopicを削除
+    removeTopic(index: Number) {
+      this.topics.splice(index, 1)
+    },
+    // topic追加
+    addTopic() {
+      this.topics.push() // 配列に１つ空データを追加する
+    },
   },
 })
 </script>
