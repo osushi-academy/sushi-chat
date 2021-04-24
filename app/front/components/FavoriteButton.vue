ba163847d03446ae03f1e9d8f4ccd5bd6857788f
knknk98 <kayooshino@gmail.com>
2021-04-24 14:04:49 +0900

    クリックしたら動くようにしようとしてる

--- app/front/components/FavoriteButton.vue
+++ app/front/components/FavoriteButton.vue
@@ -1,15 +1,25 @@
 <template>
-  <button class="stamp-submit-button heart-button" @click="clickFavorite">
+  <button class="stamp-submit-button" @click="clickFavorite">
     <span class="material-icons"> favorite </span>
+    <span v-if="show" >{{show}}</span>
   </button>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+export type DataType = {
+  show: boolean
+}
 
 export default Vue.extend({
+  data():DataType {
+    return {
+      show: true
+    }
+  },
   methods: {
     clickFavorite() {
-      this.$emit('favorite')
+      this.show != this.show;
+      this.$emit('favorite');
     },
   },
 })
