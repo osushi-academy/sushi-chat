7ec8342f152de68b8e61c028f388a968f5c44d41
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-04-24 21:20:48 +0900

    send stamp every 2 sec

--- server/src/stamp.ts
+++ server/src/stamp.ts
@@ -1,3 +1,13 @@
+import { Server } from "socket.io";
+
 export type Stamp = {
   topicId: string;
 };
+
+export function stampIntervalSender(io: Server, stamps: Stamp[]) {
+  setInterval(() => {
+    console.log(new Date());
+    io.sockets.emit("PUB_STAMP", stamps);
+    stamps.length = 0;
+  }, 2000);
+}
