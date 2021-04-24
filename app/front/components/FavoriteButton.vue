885581495826bf032693f994bec5dc1ca464845d
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-04-24 22:23:59 +0900

    ハートボタンのアニメーションを追加

--- app/front/components/FavoriteButton.vue
+++ app/front/components/FavoriteButton.vue
@@ -1,24 +1,35 @@
 <template>
   <button class="stamp-submit-button" @click="clickFavorite">
     <span class="material-icons"> favorite </span>
-    <span v-if="show" class="heart-button"></span>
+    <div v-for="c of count" :key="c">
+      <span class="heart-button"></span>
+    </div>
   </button>
 </template>
 <script lang="ts">
 import Vue from 'vue'
 export type DataType = {
   show: boolean
+  count: number[]
 }
 
 export default Vue.extend({
-  data():DataType {
+  name: 'FavoriteButton',
+  data(): DataType {
     return {
       show: false,
+      count: [],
     }
   },
   methods: {
     clickFavorite() {
-      this.show =! this.show
+      // 最後の要素に1足した値を配列に加える
+      this.count.push((this.count.slice(-1)?.[0] ?? 0) + 1)
+      // 配列の要素が50を超えたら25個消す（ハートは最大25個のみ表示する）
+      if (this.count.length > 50) {
+        this.count = this.count.slice(-25)
+      }
+
       this.$emit('favorite')
     },
   },
