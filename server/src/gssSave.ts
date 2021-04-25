c71d53e86dfa26eb0eae85594e0b5e911bd2e0ed
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-04-25 12:14:15 +0900

    oauth ver

--- server/src/gssSave.ts
+++ server/src/gssSave.ts
@@ -1,36 +1,102 @@
-import { google } from "googleapis";
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
 
-const sheets = google.sheets({
-  version: "v4",
-  auth: "hoge",
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = "token.json";
+
+// Load client secrets from a local file.
+fs.readFile("client.json", (err: any, content: any) => {
+  if (err) return console.log("Error loading client secret file:", err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listMajors);
 });
 
-sheets.spreadsheets.values.get(
-  {
-    spreadsheetId: "1vlTCapH2_R_h1FtEzrkUAIWQuMCHEs7npi_Izq0nuYY",
-    range: "シート1!A2:E",
-  },
-  (err, res) => {
-    if (err) return console.log("The API returned an error: " + err);
-    if (!!res) {
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials: any, callback: any) {
+  const { client_secret, client_id, redirect_uris } = credentials.web;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err: any, token: any) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client: any, callback: any) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Enter the code from that page here: ", (code: any) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err: any, token: any) => {
+      if (err)
+        return console.error(
+          "Error while trying to retrieve access token",
+          err
+        );
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err: any) => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Prints the names and majors of students in a sample spreadsheet:
+ * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listMajors(auth: any) {
+  const sheets = google.sheets({ version: "v4", auth });
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms",
+      range: "Class Data!A2:E",
+    },
+    (err: any, res: any) => {
+      if (err) return console.log("The API returned an error: " + err);
       const rows = res.data.values;
-      if (!!rows && rows.length) {
+      if (rows.length) {
         console.log("Name, Major:");
         // Print columns A and E, which correspond to indices 0 and 4.
-        rows.map((row) => {
+        rows.map((row: any) => {
           console.log(`${row[0]}, ${row[4]}`);
         });
       } else {
         console.log("No data found.");
       }
     }
-  }
-);
-
-sheets.spreadsheets.values.append({
-  spreadsheetId: "1vlTCapH2_R_h1FtEzrkUAIWQuMCHEs7npi_Izq0nuYY",
-  range: "シート1!A3",
-  valueInputOption: "USER_ENTERED",
-  insertDataOption: "INSERT_ROWS",
-  requestBody: { values: [["hello", "world"]] },
-});
+  );
+}
