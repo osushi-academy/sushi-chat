ae1fba36e57a0edb1d0655980aad107d7e43a696
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-04-25 11:43:16 +0900

    fix

--- app/front/pages/index.vue
+++ app/front/pages/index.vue
@@ -112,7 +112,7 @@ export default Vue.extend({
       messages: [],
       activeUserCount: 0,
       isNotify: false,
-      isAdmin: true,
+      isAdmin: false,
       icons: [
         { url: require('@/assets/img/sushi_akami.png') },
         { url: require('@/assets/img/sushi_ebi.png') },
