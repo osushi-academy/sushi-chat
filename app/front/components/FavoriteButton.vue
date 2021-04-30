a01b03285fa4e19ae93cc78cf5735db2eadd0390
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-05-01 02:20:47 +0900

    スタンプにランダム性を追加/リファクタ

--- app/front/components/FavoriteButton.vue
+++ app/front/components/FavoriteButton.vue
@@ -1,18 +1,47 @@
 <template>
-  <button
-    class="stamp-submit-button"
-    :disabled="disabled"
-    @click="clickFavorite"
-  >
-    <span class="material-icons"> favorite </span>
-    <div v-for="c of count" :key="c">
-      <span class="heart-button"></span>
+  <div class="heart-button-wraper">
+    <div v-for="c of count" :key="c.id">
+      <span
+        class="heart-button material-icons"
+        :class="{
+          'heart-button-animation': stampAnimationFinished[c.id] === false,
+        }"
+        :style="{
+          color: `hsl(${c.color.h}, ${c.color.s}%, ${c.color.l}%)`,
+          top: `${c.y}%`,
+          left: `${c.x}%`,
+        }"
+      >
+        favorite
+      </span>
+      <span
+        class="heart-button material-icons"
+        :class="{
+          'hheart-button-animation': stampAnimationFinished[c.id] === false,
+        }"
+        :style="{
+          color: `hsla(${c.color.h}, ${c.color.s}%, ${c.color.l}%, 0.5)`,
+          top: `${c.y + 10}%`,
+          left: `${c.x + 10}%`,
+        }"
+      >
+        favorite
+      </span>
     </div>
-  </button>
+
+    <button
+      class="stamp-submit-button"
+      :disabled="disabled"
+      @click="clickFavorite"
+    >
+      <span class="material-icons"> favorite </span>
+    </button>
+  </div>
 </template>
 <script lang="ts">
 import Vue, { PropOptions } from 'vue'
-import waitedLoop from '@/utils/waitedLoop'
+import { randomWaitedLoog } from '@/utils/waitedLoop'
+import { HSLColor, getRandomColor } from '@/utils/color'
 
 type FavoriteCallbackRegisterPropType = {
   favoriteCallbackRegister: (callback: (count: number) => void) => void
@@ -20,7 +49,16 @@ type FavoriteCallbackRegisterPropType = {
 
 export type DataType = {
   show: boolean
-  count: number[]
+  count: {
+    id: number
+    color: HSLColor
+    x: number
+    y: number
+    colorSequence: number
+  }[]
+  lastClicked: number
+  colorSequence: number
+  stampAnimationFinished: Record<number, boolean>
 }
 
 export default Vue.extend({
@@ -39,31 +77,64 @@ export default Vue.extend({
     return {
       show: false,
       count: [],
+      lastClicked: 0,
+      colorSequence: 0,
+      stampAnimationFinished: {},
     }
   },
   mounted() {
     this.$props.favoriteCallbackRegister((count: number) => {
-      waitedLoop(2000 / count, count, () => {
-        // 最後の要素に1足した値を配列に加える
-        this.count.push((this.count.slice(-1)?.[0] ?? 0) + 1)
-        // 配列の要素が50を超えたら25個消す（ハートは最大25個のみ表示する）
-        if (this.count.length > 50) {
-          this.count = this.count.slice(-25)
-        }
+      randomWaitedLoog(2000 / count, 500, count, () => {
+        this.emitHeart()
       })
     })
   },
   methods: {
     clickFavorite() {
       if (this.disabled) return
-      // 最後の要素に1足した値を配列に加える
-      this.count.push((this.count.slice(-1)?.[0] ?? 0) + 1)
-      // 配列の要素が50を超えたら25個消す（ハートは最大25個のみ表示する）
-      if (this.count.length > 50) {
-        this.count = this.count.slice(-25)
+      this.emitHeart()
+      this.$emit('favorite')
+    },
+    emitHeart() {
+      const colorSequence =
+        new Date().getTime() - this.lastClicked < 1000
+          ? this.colorSequence + 1
+          : 0
+
+      let key = null
+      Object.entries(this.stampAnimationFinished).forEach(([k, v]) => {
+        if (v) key = k
+      })
+
+      if (key) {
+        this.$set(this.stampAnimationFinished, key, false)
+        setTimeout(() => {
+          this.$set(this.stampAnimationFinished, key, true)
+        }, 1800)
+        return
       }
 
-      this.$emit('favorite')
+      const id = (this.count.slice(-1)?.[0]?.id ?? 0) + 1
+
+      // 最後の要素に1足した値を配列に加える
+      this.count.push({
+        id,
+        color: getRandomColor(colorSequence),
+        x: Math.floor(Math.random() * 40),
+        y: Math.floor(Math.random() * 0),
+        colorSequence,
+      })
+      // 配列の要素が100を超えたらリセットする
+      if (this.count.length > 100) {
+        this.count = []
+      }
+
+      this.lastClicked = new Date().getTime()
+      this.colorSequence = colorSequence
+      this.$set(this.stampAnimationFinished, id, false)
+      setTimeout(() => {
+        this.$set(this.stampAnimationFinished, id, true)
+      }, 1800)
     },
   },
 })
---
+++ app/front/utils/color.ts
@@ -0,0 +1,10 @@
+export type HSLColor = {
+  h: number
+  s: number
+  l: number
+}
+
+export const getRandomColor = (colorSequence: number): HSLColor => {
+  const h = (colorSequence * 4) % 100
+  return { h: h < 50 ? h : 100 - h, s: 80, l: 60 }
+}
