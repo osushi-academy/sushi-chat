8ecc5852884302b8bde943d6218728a0614320a7
knknk98 <kayooshino@gmail.com>
2021-05-03 02:16:03 +0900

    管理画面内の大まかなレイアウト

--- app/front/components/SettingPage.vue
+++ app/front/components/SettingPage.vue
@@ -1,3 +1,56 @@
 <template>
-  <div class="drawer-menu-wrapper">管理画面だよ</div>
+  <div class="drawer-menu-wrapper">
+    <div class="drawer-menu">
+      <div class="header">
+        <p class="room-title">技育CAMPハッカソンvol.114514</p>
+        <div class="room-url">
+          <p>
+            htpps://www.sushi-chat.com
+            <button class="material-icons">content_copy</button>
+          </p>
+        </div>
+        <button>次のトピックに遷移</button>
+        <div class="my-icon icon-wrapper">
+          <img src="@/assets/img/sushi_akami.png" alt="" />
+        </div>
+      </div>
+      <div class="topic-list">
+        <ol>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+          <li>チームA</li>
+          <li>チームB</li>
+          <li>チームC</li>
+        </ol>
+      </div>
+    </div>
+  </div>
 </template>
