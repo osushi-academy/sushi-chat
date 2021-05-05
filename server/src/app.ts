0c250ca10f2261e7ebf4f94c0b27d11af8c94e34
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-05-05 22:35:53 +0900

    create database file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createServer } from "http";
-import { clientCheck } from "./database";
+import { clientCheck } from "./database/database";
 import createSocketIOServer from "./ioServer";
 
 const app = express();
