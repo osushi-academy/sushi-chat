5fca5cee9f5747e6521dceae7fdfb1a4a380edc4
knknk98 <kayooshino@gmail.com>
2021-05-09 03:59:14 +0900

    新しいモデルに対応（繋ぎ込みはまだ）

--- app/front/components/SettingPage.vue
+++ app/front/components/SettingPage.vue
@@ -3,14 +3,14 @@
     <div class="drawer-menu">
       <div class="header">
         <div class="icon-space">
-          <img class="icon-wrapper" src="@/assets/img/sushi_akami.png" alt="" />
+          <img class="icon-wrapper" :src="icon" alt="" />
         </div>
         <div class="room-info">
           <div class="room-title">
-            <p>ハッカソンvol.12345678901234567890</p>
+            <p>{{ room.title }}</p>
           </div>
           <div class="room-url">
-            <span>{{ test }}</span>
+            <span>https://sushi-chat-cyan.vercel.app/{{ room.roomKey }}</span>
             <button
               class="material-icons copy-button"
               @click="writeToClipboard"
@@ -21,71 +21,46 @@
         </div>
       </div>
 
-      <button class="next-topic-button">
+      <button v-if="MyIconId === '0'" class="next-topic-button">
         <span class="material-icons"> fast_forward </span>
         次のトピックに遷移
       </button>
 
       <div class="topic-list">
-        <div class="topic">
-          <div class="topic-number">1</div>
+        <div
+          v-for="(topic, index) in room.topics"
+          :key="topic.id"
+          class="topic"
+          :class="topicStates[topic.id]"
+        >
+          <div class="topic-number">{{ index + 1 }}</div>
           <div class="topic-name">
-            チーム名long long long long long long long<span class="label"
+            {{ topic.title
+            }}<span v-if="topicStates[topic.id] === 'ongoing'" class="label"
               >進行中</span
             >
+            <span v-if="topicStates[topic.id] === 'paused'" class="label"
+              >一時停止</span
+            >
           </div>
-          <div class="buttons">
-            <button><span class="material-icons">play_arrow</span></button>
-            <button><span class="material-icons">stop</span></button>
-          </div>
-        </div>
-        <div class="topic">
-          <div class="topic-number">10</div>
-          <div class="topic-name">チーム名</div>
-          <div class="buttons">
-            <button><span class="material-icons">play_arrow</span></button>
-            <button><span class="material-icons">stop</span></button>
-          </div>
-        </div>
-        <div class="topic">
-          <div class="topic-number">10</div>
-          <div class="topic-name">チーム名</div>
-          <div class="buttons">
-            <button><span class="material-icons">play_arrow</span></button>
-            <button><span class="material-icons">stop</span></button>
-          </div>
-        </div>
-        <div class="topic">
-          <div class="topic-number">10</div>
-          <div class="topic-name">チーム名</div>
-          <div class="buttons">
-            <button><span class="material-icons">play_arrow</span></button>
-            <button><span class="material-icons">stop</span></button>
-          </div>
-        </div>
-        <div class="topic">
-          <div class="topic-number">10</div>
-          <div class="topic-name">チーム名</div>
-          <div class="buttons">
-            <button><span class="material-icons">play_arrow</span></button>
-            <button><span class="material-icons">stop</span></button>
-          </div>
-        </div>
-        <div class="topic">
-          <div class="topic-number">10</div>
-          <div class="topic-name">チーム名</div>
-          <div class="buttons">
-            <button><span class="material-icons">play_arrow</span></button>
-            <button><span class="material-icons">stop</span></button>
-          </div>
-        </div>
-
-        <div class="topic">
-          <div class="topic-number">100</div>
-          <div class="topic-name">チーム名</div>
-          <div class="buttons">
-            <button><span class="material-icons">play_arrow</span></button>
-            <button><span class="material-icons">stop</span></button>
+          <div v-if="MyIconId === '0'" class="buttons">
+            <button>
+              <span
+                v-if="topicStates[topic.id] != 'finished'"
+                class="material-icons"
+                >{{ playOrPause(topicStates[topic.id]) }}</span
+              >
+            </button>
+            <button>
+              <span
+                v-if="
+                  topicStates[topic.id] === 'ongoing' ||
+                  topicStates[topic.id] === 'paused'
+                "
+                class="material-icons"
+                >stop</span
+              >
+            </button>
           </div>
         </div>
       </div>
@@ -96,8 +71,26 @@
 <script lang="ts">
 import Vue from 'vue'
 
+// TODO:モデル更新後にここは消してcontent.ts読み込むようにする
+type TopicState = 'not-started' | 'ongoing' | 'paused' | 'finished'
+type TopicLinkType = 'github' | 'slide' | 'product'
+type Topic = {
+  id: string
+  title: string
+  description: string
+  urls: Record<TopicLinkType, string>
+}
+type Room = {
+  id: string
+  roomKey: string
+  title: string
+  topics: Topic[]
+}
+
 type DataType = {
-  test: string
+  room: Room
+  topicStates: { [key: string]: TopicState }
+  MyIconId: string
 }
 
 export default Vue.extend({
@@ -105,15 +98,118 @@ export default Vue.extend({
   props: {},
   data(): DataType {
     return {
-      test: 'https://sushi-chat-cyan.vercel.app/hogehogehoge',
+      room: {
+        id: 'testID',
+        roomKey: 'testRoomKey',
+        title: '技育CAMPハッカソンvol.3',
+        topics: DUMMY_TOPICS,
+      },
+      topicStates: DUMMY_TOPIC_STATES,
+      MyIconId: '0',
     }
+  },
+  computed: {
+    icon(): { icon: string } {
+      return ICONS[this.MyIconId]?.icon ?? ICONS[0].icon
+    },
+    playOrPause(): {} {
+      return function (type: string) {
+        if (type === 'ongoing' || type === 'not-started') {
+          return 'play_arrow'
+        } else if (type === 'paused') {
+          return 'pause'
+        } else {
+          return null
+        }
+      }
+    },
   },
   methods: {
     writeToClipboard() {
-      navigator.clipboard.writeText(this.test).catch((e) => {
-        console.error(e)
-      })
+      navigator.clipboard.writeText(this.test)
     },
   },
 })
+
+const DUMMY_TOPICS = [
+  {
+    id: 'a',
+    title: 'チームA',
+    description: 'aaaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+  {
+    id: 'b',
+    title: 'チームB',
+    description: 'aaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+  {
+    id: 'c',
+    title: 'チームCCCCCCCCCCCCCCCCC',
+    description: 'aaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+  {
+    id: 'd',
+    title: 'チームDDDDDDDDDDDDDDDDDDDD',
+    description: 'aaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+  {
+    id: 'e',
+    title: 'チームE',
+    description: 'aaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+  {
+    id: 'f',
+    title: 'チームF',
+    description: 'aaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+  {
+    id: 'g',
+    title: 'チームG',
+    description: 'aaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+  {
+    id: 'h',
+    title: 'チームH',
+    description: 'aaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+  {
+    id: 'i',
+    title: 'チームI',
+    description: 'aaaaa',
+    urls: { github: '', slide: '', product: '' },
+  },
+]
+const DUMMY_TOPIC_STATES: { [key: string]: TopicState } = {
+  a: 'finished',
+  b: 'finished',
+  c: 'ongoing',
+  d: 'paused',
+  e: 'not-started',
+  f: 'not-started',
+  g: 'not-started',
+  h: 'not-started',
+  i: 'not-started',
+}
+const ICONS = [
+  { icon: require('@/assets/img/tea.png') },
+  { icon: require('@/assets/img/sushi_akami.png') },
+  { icon: require('@/assets/img/sushi_ebi.png') },
+  { icon: require('@/assets/img/sushi_harasu.png') },
+  { icon: require('@/assets/img/sushi_ikura.png') },
+  { icon: require('@/assets/img/sushi_iwashi.png') },
+  { icon: require('@/assets/img/sushi_kai_hokkigai.png') },
+  { icon: require('@/assets/img/sushi_salmon.png') },
+  { icon: require('@/assets/img/sushi_shirasu.png') },
+  { icon: require('@/assets/img/sushi_tai.png') },
+  { icon: require('@/assets/img/sushi_uni.png') },
+  { icon: require('@/assets/img/sushi_syari.png') },
+]
 </script>
