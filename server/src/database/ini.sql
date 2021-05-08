ffb0f014d2f8148d021ac527a791fb16e17814fa
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-05-09 02:52:41 +0900

    change db def

--- server/src/database/ini.sql
+++ server/src/database/ini.sql
@@ -17,44 +17,56 @@ CREATE TABLE Topics (
     productUrl TEXT
 );
 
-CREATE TABLE Messages (
+CREATE TABLE ChatItems (
   id UUID NOT NULL PRIMARY KEY,
+  type INT NOT NULL,
   roomId UUID NOT NULL REFERENCES Rooms(id),
   topicId UUID NOT NULL REFERENCES Topics(id),
   iconId VARCHAR(10) NOT NULL,
   timestamp INT NOT NULL,
   createdAt TIMESTAMP NOT NULL,
-  content TEXT NOT NULL,
-  targetId UUID
-);
+  content TEXT,
+  targetId UUID REFERENCES ChatItems(id)
+)
 
-CREATE TABLE Reactions (
-  id UUID NOT NULL PRIMARY KEY,
-  roomId UUID NOT NULL REFERENCES Rooms(id),
-  topicId UUID NOT NULL REFERENCES Topics(id),
-  iconId VARCHAR(10) NOT NULL,
-  timestamp INT NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  targetId UUID NOT NULL REFERENCES Messages(id)
-);
+-- CREATE TABLE Messages (
+--   id UUID NOT NULL PRIMARY KEY,
+--   roomId UUID NOT NULL REFERENCES Rooms(id),
+--   topicId UUID NOT NULL REFERENCES Topics(id),
+--   iconId VARCHAR(10) NOT NULL,
+--   timestamp INT NOT NULL,
+--   createdAt TIMESTAMP NOT NULL,
+--   content TEXT NOT NULL,
+--   targetId UUID
+-- );
 
-CREATE TABLE Questions (
-  id UUID NOT NULL PRIMARY KEY,
-  roomId UUID NOT NULL REFERENCES Rooms(id),
-  topicId UUID NOT NULL REFERENCES Topics(id),
-  iconId VARCHAR(10) NOT NULL,
-  timestamp INT NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  content TEXT NOT NULL
-);
+-- CREATE TABLE Reactions (
+--   id UUID NOT NULL PRIMARY KEY,
+--   roomId UUID NOT NULL REFERENCES Rooms(id),
+--   topicId UUID NOT NULL REFERENCES Topics(id),
+--   iconId VARCHAR(10) NOT NULL,
+--   timestamp INT NOT NULL,
+--   createdAt TIMESTAMP NOT NULL,
+--   targetId UUID NOT NULL REFERENCES Messages(id)
+-- );
 
-CREATE TABLE Answers (
-  id UUID NOT NULL PRIMARY KEY,
-  roomId UUID NOT NULL REFERENCES Rooms(id),
-  topicId UUID NOT NULL REFERENCES Topics(id),
-  iconId VARCHAR(10) NOT NULL,
-  timestamp INT NOT NULL,
-  createdAt TIMESTAMP NOT NULL,
-  content TEXT NOT NULL,
-  targetId UUID REFERENCES Questions(id)
-);
+-- CREATE TABLE Questions (
+--   id UUID NOT NULL PRIMARY KEY,
+--   roomId UUID NOT NULL REFERENCES Rooms(id),
+--   topicId UUID NOT NULL REFERENCES Topics(id),
+--   iconId VARCHAR(10) NOT NULL,
+--   timestamp INT NOT NULL,
+--   createdAt TIMESTAMP NOT NULL,
+--   content TEXT NOT NULL
+-- );
+
+-- CREATE TABLE Answers (
+--   id UUID NOT NULL PRIMARY KEY,
+--   roomId UUID NOT NULL REFERENCES Rooms(id),
+--   topicId UUID NOT NULL REFERENCES Topics(id),
+--   iconId VARCHAR(10) NOT NULL,
+--   timestamp INT NOT NULL,
+--   createdAt TIMESTAMP NOT NULL,
+--   content TEXT NOT NULL,
+--   targetId UUID REFERENCES Questions(id)
+-- );
