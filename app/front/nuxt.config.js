83608c9ae8fedfb02ae285d4e9440f5f444a572f
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-05-29 19:51:56 +0900

    [fix] 選択できるテキストの変更、ダブルクリックで拡大の対応

--- app/front/nuxt.config.js
+++ app/front/nuxt.config.js
@@ -17,6 +17,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no',
+      },
       { hid: 'description', name: 'description', content: '' },
       {
         hid: 'og:url',
