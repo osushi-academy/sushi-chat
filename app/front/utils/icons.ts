8f0a940712a0659062ae9d5b1d5110a9ed1904fd
knknk98 <kayooshino@gmail.com>
2021-08-10 01:38:20 +0900

    ICONSの定義にwebp追加

--- app/front/utils/icons.ts
+++ app/front/utils/icons.ts
@@ -1,18 +1,18 @@
 import { IconsDataType } from '@/models/contents'
 
 const ICONS:IconsDataType = [
-    { png: require('@/assets/img/tea.png') },
-    { png: require('@/assets/img/sushi_akami.png') },
-    { png: require('@/assets/img/sushi_ebi.png') },
-    { png: require('@/assets/img/sushi_harasu.png') },
-    { png: require('@/assets/img/sushi_ikura.png') },
-    { png: require('@/assets/img/sushi_iwashi.png') },
-    { png: require('@/assets/img/sushi_kai_hokkigai.png') },
-    { png: require('@/assets/img/sushi_salmon.png') },
-    { png: require('@/assets/img/sushi_shirasu.png') },
-    { png: require('@/assets/img/sushi_tai.png') },
-    { png: require('@/assets/img/sushi_uni.png') },
-    { png: require('@/assets/img/sushi_syari.png') },
+    { png: require('@/assets/img/tea.png'), webp:require('@/assets/img/tea.webp') },
+    { png: require('@/assets/img/sushi_akami.png'), webp: require('@/assets/img/sushi_akami.webp') },
+    { png: require('@/assets/img/sushi_ebi.png'), webp: require('@/assets/img/sushi_ebi.webp') },
+    { png: require('@/assets/img/sushi_harasu.png'), webp: require('@/assets/img/sushi_harasu.webp') },
+    { png: require('@/assets/img/sushi_ikura.png'), webp: require('@/assets/img/sushi_ikura.webp') },
+    { png: require('@/assets/img/sushi_iwashi.png'), webp: require('@/assets/img/sushi_iwashi.webp') },
+    { png: require('@/assets/img/sushi_kai_hokkigai.png'), webp: require('@/assets/img/sushi_kai_hokkigai.webp') },
+    { png: require('@/assets/img/sushi_salmon.png'), webp: require('@/assets/img/sushi_salmon.webp') },
+    { png: require('@/assets/img/sushi_shirasu.png'), webp: require('@/assets/img/sushi_shirasu.webp') },
+    { png: require('@/assets/img/sushi_tai.png'), webp: require('@/assets/img/sushi_tai.webp') },
+    { png: require('@/assets/img/sushi_uni.png'), webp: require('@/assets/img/sushi_uni.webp') },
+    { png: require('@/assets/img/sushi_syari.png'), webp: require('@/assets/img/sushi_syari.webp') },
 ]
 
 export default ICONS
