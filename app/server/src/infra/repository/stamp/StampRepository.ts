bce391cc725844081b941b3df9ff6d1232fbe5b0
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-09 18:04:26 +0900

    Added count() to IStampRepository
    Stampがちゃんと保存されるかのテストするのに、保存されているスタンプ数を知るメソッドが欲しかったので作りました。

--- app/server/src/infra/repository/stamp/StampRepository.ts
+++ app/server/src/infra/repository/stamp/StampRepository.ts
@@ -16,11 +16,38 @@ class StampRepository implements IStampRepository {
       ])
     } catch (e) {
       console.error(
-        `${e.message ?? "Unknown error."} (SAVE ROOM(${
-          stamp.roomId
-        })/STAMP(userId: ${stamp.userId}, roomId: ${stamp.roomId}, topicId: ${
-          stamp.topicId
-        }) IN DB)`,
+        `${e.message ?? "Unknown error."} (SAVE STAMP(userId: ${
+          stamp.userId
+        }, roomId: ${stamp.roomId}, topicId: ${stamp.topicId}))`,
+        new Date().toISOString(),
+      )
+
+      throw e
+    }
+  }
+
+  public async count(
+    roomId: string,
+    topicId: string,
+    userId?: string,
+  ): Promise<number> {
+    try {
+      const query =
+        "SELECT COUNT(*) FROM stamps WHERE roomid = $1 AND topicid = $2" +
+        userId
+          ? " AND userid = $3"
+          : ""
+      return (
+        await this.pgClient.query(
+          query,
+          userId ? [roomId, topicId, userId] : [roomId, topicId],
+        )
+      ).rows[0].count as number
+    } catch (e) {
+      console.error(
+        `${e.message ?? "Unknown error."} (COUNT STAMP(${
+          userId && "userId: " + userId + ", "
+        }roomId: ${roomId}, topicId: ${topicId}) IN DB)`,
         new Date().toISOString(),
       )
 
