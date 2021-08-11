b9f4ba51fdca112748cef711944dd40231eaabf1
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-11 23:01:02 +0900

    Revert "Enabled roomId() and iconId() of User return also null."
    This reverts commit 39f904b37faece07693fa3e09a6afdaaab91b629.
    
    別ブランチにて対応する

--- app/server/src/domain/user/User.ts
+++ app/server/src/domain/user/User.ts
@@ -1,16 +1,24 @@
 class User {
   public static readonly ADMIN_ICON_ID = "0"
 
-  private _roomId: string | null = null
-  private _iconId: string | null = null
+  private _roomId: string | undefined
+  private _iconId: string | undefined
 
   constructor(public readonly id: string) {}
 
-  public get roomId(): string | null {
+  public get roomId(): string {
+    if (this._roomId === undefined) {
+      throw new Error("Can't call roomId of User whose roomId is empty.")
+    }
+
     return this._roomId
   }
 
-  public get iconId(): string | null {
+  public get iconId(): string {
+    if (this._iconId === undefined) {
+      throw new Error("Can't call iconId of User whose iconId is empty.")
+    }
+
     return this._iconId
   }
 
