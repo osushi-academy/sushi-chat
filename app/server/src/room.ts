d4ca4b056b29ba474cab139309504bf1da684807
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-16 11:00:50 +0900

    Removed unused type definition

--- app/server/src/room.ts
+++ app/server/src/room.ts
@@ -1,37 +1,7 @@
-import { ChatItem } from "./chatItem"
 import Topic from "./domain/room/Topic"
 
 export type Room = {
   id: string
   title: string
-  topics: Topic[]
-}
-
-export type BuildRoom = {
-  topics: Topic[]
-}
-
-export type BuildRoomReceive = {
-  topics: Topic[]
-}
-
-export type EnterRoom = {
-  iconId: number // 新しいユーザーのアイコンID
-  activeUserCount: number // 現在入室中のユーザー数（新しいユーザーを含む）
-}
-
-export type LeaveRoom = {
-  iconId: number // 退室したユーザーのアイコンID
-  activeUserCount: number // 現在入室中のユーザー数（ユーザーが退室した後の人数）
-}
-
-export type EnterRoomResponce = {
-  chatItems: ChatItem[] // 現在までのチャット履歴（stringはtopicIdが入る）
-  topics: Topic[] // トピック情報
-  activeUserCount: number // 現在入室中のユーザー数
-}
-
-export type EnterRoomReceive = {
-  iconId: string // アイコンID
-  roomId: string // 部屋のID [Option]
+  topics: Omit<Topic, "state">[]
 }
