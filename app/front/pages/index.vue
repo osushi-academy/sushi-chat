d9574a7a8cb9e1af7f030e1543a3e301c113812a
knknk98 <kayooshino@gmail.com>
2021-08-21 00:03:30 +0900

    ヘッダーの基本形

--- app/front/pages/index.vue
+++ app/front/pages/index.vue
@@ -18,15 +18,7 @@
         @click-icon="clickIcon"
         @hide-modal="hide"
       />
-      <SidebarDrawer
-        :title="'技育CAMPハッカソン vol.5'"
-        :description="'2日間(事前開発OK)で成果物を創ってエンジニアとしてレベルアップするオンラインハッカソン。テーマは「無駄開発」。'"
-        :topics="[
-          { id: 1, label: 'おすしアカデミー' },
-          { id: 2, label: '量子力学セミナー' },
-          { id: 3, label: 'だしまきたまご' },
-        ]"
-      ></SidebarDrawer>
+
       <SettingPage
         v-if="isDrawer && isAdmin"
         :room-id="room.id"
