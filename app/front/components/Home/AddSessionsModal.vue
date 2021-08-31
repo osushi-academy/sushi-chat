b38938af199bd9cc42900452f93314d3a43a16d1
knknk98 <kayooshino@gmail.com>
2021-09-01 00:22:56 +0900

    イベント作成画面モーダル(ハリボテ)

--- app/front/components/Home/AddSessionsModal.vue
+++ app/front/components/Home/AddSessionsModal.vue
@@ -1,24 +1,54 @@
 <template>
-  <modal
-    class="home-add-sessions-modal"
-    name="home-add-sessions-modal"
-    adaptive
-  >
-    セッション名を入力してください
-    <button
-      class="home-add-sessions-modal__completed-button"
-      @click="$modal.hide('home-add-sessions-modal')"
-    >
-      OK
-    </button>
-  </modal>
+  <Modal name="home-add-sessions-modal" class="home-add-sessions-modal">
+    <template #title>セッション名を入力してください</template>
+    <template #content>
+      <div class="home-add-sessions-modal__separators">
+        <div class="home-add-sessions-modal__separators--new-line">
+          <input
+            id="new-line"
+            type="radio"
+            name="separator"
+            value="new-line"
+            checked
+          />
+          <label for="new-line">改行で区切る</label>
+        </div>
+        <div class="home-add-sessions-modal__separators--comma">
+          <input
+            id="comma"
+            type="radio"
+            name="separator"
+            value="comma"
+            checked
+          />
+          <label for="comma">カンマで区切る</label>
+        </div>
+        <div class="home-add-sessions-modal__separators--blank">
+          <input
+            id="blank"
+            type="radio"
+            name="separator"
+            value="blank"
+            checked
+          />
+          <label for="blank">空白で区切る</label>
+        </div>
+      </div>
+      <textarea class="home-add-sessions-modal__textarea" />
+    </template>
+    <template #hide-button class="home-add-sessions-modal__button">OK</template>
+  </Modal>
 </template>
 
 <script lang="ts">
 import Vue from "vue"
+import Modal from "@/components/Home/Modal.vue"
 
 export default Vue.extend({
   name: "HomeAddSessionsModal",
+  components: {
+    Modal,
+  },
   layout: "home",
 })
 </script>
