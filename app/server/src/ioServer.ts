a3cbc6b8a4a1efcb9c5deeccfdc2d10c9199dbcc
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-08-31 11:35:55 +0900

    use redis adapter
    複数サーバーにしたときに、サーバー間でSocketIOのメッセージをやりとりするためにRedisのアダプターを導入した。詳しくは公式ドキュメントを参照(https://socket.io/docs/v4/redis-adapter/)

--- app/server/src/ioServer.ts
+++ app/server/src/ioServer.ts
@@ -3,6 +3,7 @@ import { Server as HttpServer } from "http"
 import { v4 as uuid } from "uuid"
 import { ReceiveEventParams, ReceiveEventResponses } from "./events"
 import { instrument } from "@socket.io/admin-ui"
+import { createAdapter } from "@socket.io/redis-adapter"
 import { generateHash } from "./utils/crypt"
 import RoomService from "./service/room/RoomService"
 import StampService from "./service/stamp/StampService"
@@ -17,6 +18,7 @@ import IStampRepository from "./domain/stamp/IStampRepository"
 import ChatItemDelivery from "./infra/delivery/chatItem/ChatItemDelivery"
 import RoomDelivery from "./infra/delivery/room/RoomDelivery"
 import UserDelivery from "./infra/delivery/user/UserDelivery"
+import { createClient } from "redis"
 
 const createSocketIOServer = async (
   httpServer: HttpServer,
@@ -27,10 +29,19 @@ const createSocketIOServer = async (
 ) => {
   const io = new Server(httpServer, {
     cors: {
-      origin: "*",
+      origin: process.env.CORS_ORIGIN ?? "http://localhost:3000",
       methods: ["GET", "POST"],
+      credentials: true,
     },
   })
+
+  const pubClient = createClient({
+    host: process.env.REDIS_HOST ?? "localhost",
+    port: parseInt(process.env.REDIS_PORT ?? "6379"),
+  })
+  const subClient = pubClient.duplicate()
+  io.adapter(createAdapter(pubClient, subClient))
+
   if (
     process.env.NODE_ENV == "production" &&
     process.env.SOCKET_IO_ADMIN_UI_PASSWORD === undefined
