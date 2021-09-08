26984da1093c61036c58a958452ad074876f8eb1
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-09-08 21:38:29 +0900

    Revert "サーバーを複数台構成にし、mainブランチの更新時に自動デプロイするようにした"

--- scripts/mod_permission.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /app/app/server || exit 1
-chmod -R 700 dist && chown -R app:app dist
--- scripts/start_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /app/app/server || exit 1
-NODE_ENV=production yarn start > /var/log/app/info.log 2> /var/log/app/error.log < /dev/null &
