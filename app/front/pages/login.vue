913462cd73e262961ed73f5e0b0d089c73fa17c5
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-09-14 22:13:59 +0900

    ログインページとプロフィールページを実装

---
+++ app/front/pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="signInWithGoogle">ログイン</button>
+</template>
+<script lang="ts">
+import Vue from "vue"
+export default Vue.extend({
+  methods: {
+    async signInWithGoogle() {
+      const provider = new this.$fireModule.auth.GoogleAuthProvider()
+      try {
+        await this.$fire.auth.signInWithPopup(provider)
+        console.log("LOGIN SUCCESS")
+      } catch (e) {
+        alert("ログインに失敗しました")
+        console.error(e)
+      }
+    },
+  },
+})
+</script>
