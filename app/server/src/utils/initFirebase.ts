45d90295732e80c148010e450bcbca634b1cd912
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-09-15 22:18:39 +0900

    add comment

--- app/server/src/utils/initFirebase.ts
+++ app/server/src/utils/initFirebase.ts
@@ -3,6 +3,7 @@ import * as admin from "firebase-admin"
 const cert = {
   projectId: process.env.FIREBASE_ADMIN_PROJECT_ID ?? "",
   clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL ?? "",
+  // NOTE: 改行コードを変換する（参考 https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse）
   privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY ?? "").replace(
     /\\n/g,
     "\n",
