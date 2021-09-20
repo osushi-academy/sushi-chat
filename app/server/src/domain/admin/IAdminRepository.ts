e507c34b8d1de44963837edf15fe982608712352
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-09-20 21:53:07 +0900

    admin's managedRoomIds is not null

--- app/server/src/domain/admin/IAdminRepository.ts
+++ app/server/src/domain/admin/IAdminRepository.ts
@@ -2,7 +2,7 @@ import Admin from "./admin"
 
 interface IAdminRepository {
   find(adminId: string): Promise<Admin | null>
-  selectByRoomId(roomId: string): Promise<Admin[]>
+  selectIdsByRoomId(roomId: string): Promise<string[]>
 }
 
 export default IAdminRepository
--- app/server/src/domain/admin/admin.ts
+++ app/server/src/domain/admin/admin.ts
@@ -2,8 +2,7 @@ class Admin {
   constructor(
     public readonly id: string,
     public readonly name: string,
-    // FIXME: repositoryの実装の都合状、adminの一覧取得の際にはundefinedになるようになっている
-    public readonly managedRoomsIds?: string[],
+    public readonly managedRoomsIds: string[],
   ) {}
 }
 
