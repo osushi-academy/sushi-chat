c078aecb7cf81c83d44a12b65def45ceaeda8eb6
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-21 18:08:54 +0900

    del getter and make public readonly

--- app/server/src/domain/user/User.ts
+++ app/server/src/domain/user/User.ts
@@ -5,22 +5,10 @@ class User {
 
   constructor(
     public readonly id: string,
-    private _roomId: string,
-    private _iconId: IconId,
-    private _isAdmin: boolean,
+    public readonly roomId: string,
+    public readonly iconId: IconId,
+    public readonly isAdmin: boolean,
   ) {}
-
-  public get roomId(): string {
-    return this._roomId
-  }
-
-  public get iconId(): IconId {
-    return this._iconId
-  }
-
-  public get isAdmin(): boolean {
-    return this._isAdmin
-  }
 }
 
 export default User
