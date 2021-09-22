e7e0891815d8cd109039c09222e4ebf7d7d9afde
knknk98 <kayooshino@gmail.com>
2021-09-23 02:33:44 +0900

    寿司選択画面404UI

---
+++ app/front/components/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sushi-select not-found">
+    <picture class="not-found__icon">
+      <source :srcset="icon.webp" type="image/webp" />
+      <img :src="icon.png" alt="" />
+    </picture>
+    <div class="not-found__textbox">
+      <div class="not-found__textbox--title">
+        お探しのルームが見つかりません
+      </div>
+      <div>お探しのルームは、公開が停止されたか削除された可能性があります</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import ICONS from "@/utils/icons"
+
+export default Vue.extend({
+  name: "NotFound",
+  computed: {
+    icon() {
+      return ICONS[0]
+    },
+  },
+})
+</script>
--- app/front/components/SelectIconModal.vue
+++ app/front/components/SelectIconModal.vue
@@ -97,5 +97,3 @@ export default Vue.extend({
   },
 })
 </script>
-
-<style lang="scss" scoped></style>
