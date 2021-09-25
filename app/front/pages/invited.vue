4dda46a0515d47274428b7acb27326a3b8f1988c
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-09-25 21:56:44 +0900

    add

--- app/front/pages/invited.vue
+++ app/front/pages/invited.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="container page">
     <main>
-      <p>招待されています</p>
+      {{ room.title }}
+      {{ room.description }}
+      {{ room.state }}
+      <p>管理者に招待されています</p>
+      <button @click="regiaterAdmin">登録する</button>
     </main>
   </div>
 </template>
@@ -10,32 +14,38 @@
 import Vue from "vue"
 import VModal from "vue-js-modal"
 import { RoomModel } from "~/../shared/dist"
-import { DeviceStore, UserItemStore } from "~/store"
 
 // Data型
 type DataType = {
   // ルーム情報
   room: RoomModel
   roomId: string
+  adminInviteKey: string
 }
 Vue.use(VModal)
 export default Vue.extend({
   name: "Invited",
-  async asyncData({ app }) {
+  async asyncData({ app, query }) {
     const res = await app.$apiClient.get(
       {
         pathname: "/room/:id",
-        params: { id: roomId },
+        params: { id: query.roomId as string },
       },
       {},
     )
     if (res.result === "error") {
-      throw new Error("招待失敗")
+      throw new Error("ルーム情報なし")
     }
     return { room: res.data }
   },
   data(): DataType {
-    return { room: null, roomId: "" }
+    return { room: {} as RoomModel, roomId: "", adminInviteKey: "" }
+  },
+  created(): any {
+    // roomId取得
+    this.roomId = this.$route.query.roomId as string
+    // adminInviteKey取得
+    this.adminInviteKey = this.$route.query.adminInviteKey as string
   },
   mounted(): any {
     // roomId取得
@@ -43,6 +53,24 @@ export default Vue.extend({
     if (this.roomId !== "") {
       // TODO: this.room.idが存在しない→404
     }
+  },
+  methods: {
+    async regiaterAdmin() {
+      const res = await this.$apiClient.post(
+        {
+          pathname: "/room/:id/invited?admin_invite_key=:adminInviteKey",
+          params: {
+            id: this.roomId as string,
+            adminInviteKey: this.adminInviteKey,
+          },
+        },
+        {},
+      )
+      if (res.result === "error") {
+        throw new Error("ルーム情報なし")
+      }
+      return { room: res.data }
+    },
   },
 })
 </script>
