878d2cacae04113704bffaaf818855e1e7d1870c
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-09-26 13:41:29 +0900

    fix IconId validation to ues shari

--- app/server/src/domain/user/IconId.ts
+++ app/server/src/domain/user/IconId.ts
@@ -3,7 +3,7 @@ interface IconId extends Number {
 }
 
 export const NewIconId = (value: number): IconId => {
-  if (value < 0 || value > 10) {
+  if (value < 0 || value > 11) {
     throw new Error(`value(${value}) is invalid for IconId: out of 0 to 10.`)
   }
   return value as any
