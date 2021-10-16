f64388ab306b7189bd64db0aaba593cb529a6926
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-10-17 00:05:04 +0900

    リダイレクト先を修正

--- app/front/middleware/privateRoute.ts
+++ app/front/middleware/privateRoute.ts
@@ -2,7 +2,7 @@ import { Middleware } from "@nuxt/types"
 
 const privateRoute: Middleware = ({ store, redirect }) => {
   if (!store.getters["auth/isLoggedIn"]) {
-    return redirect("/login")
+    return redirect("/")
   }
 }
 
