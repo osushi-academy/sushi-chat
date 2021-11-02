6a1a9f03c87c26d3d9862a4961739693724728ee
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-11-02 16:06:16 +0900

    cp node_modules

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -5,6 +5,8 @@ cd /app || exit 1
 yarn build:shared
 
 mkdir -p /out/app/server
+cp -r node_modules /out
+cp -r app/server/node_modules /out/app/server
 yarn build:server --outDir /out/app/server/dist
 
 cp app/server/.env /out/app/server/.env
