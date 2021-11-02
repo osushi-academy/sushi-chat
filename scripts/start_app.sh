0179551204c98a86b9449dd64075f634ba8d0102
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-11-02 22:02:29 +0900

    fix start app script

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-NODE_ENV=production node --require dotenv/config /out/app/server/dist/src/app.js >/var/log/app/info.log 2>/var/log/app/error.log </dev/null &
+cd /out/app/server || exit 1
+NODE_ENV=production node --require dotenv/config ./dist/src/app.js >/var/log/app/info.log 2>/var/log/app/error.log </dev/null &
