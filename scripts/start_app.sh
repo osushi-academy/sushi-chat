e1ef6107028ae1121138c644c3fd0ab8722188b5
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-11-06 10:20:36 +0900

    rollback deploy scripts

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /out/app/server || exit 1
+cd /app/app/server || exit 1
 NODE_ENV=production node --require dotenv/config ./dist/src/app.js >/var/log/app/info.log 2>/var/log/app/error.log </dev/null &
