e5389db1f9c1182cccd568fd4d4a60f55e024fdd
Erina Yamakawa <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2021-11-28 22:43:30 +0900

    add message varidation test

--- app/server/src/__test__/testChatItemService.ts
+++ app/server/src/__test__/testChatItemService.ts
@@ -163,6 +163,20 @@ describe("ChatItemServiceのテスト", () => {
       expect(deliveredMessage.content).toBe("テストメッセージ")
       expect(deliveredMessage.quote?.id).toBe(target.id)
     })
+
+    test("異常系_300文字を超えるmessageを投稿", async () => {
+      const messageId = uuid()
+      await expect(() =>
+        chatItemService.postMessage({
+          chatItemId: messageId,
+          userId,
+          topicId: 1,
+          content:
+            "親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事が",
+          quoteId: null,
+        }),
+      ).rejects.toThrowError()
+    })
   })
 
   describe("postReactionのテスト", () => {
@@ -236,6 +250,20 @@ describe("ChatItemServiceのテスト", () => {
       expect(deliveredQuestion.isPinned).toBeFalsy()
       expect(deliveredQuestion.content).toBe("テストクエスチョン")
     })
+
+    test("異常系_300文字を超えるquestionを投稿", async () => {
+      const messageId = uuid()
+      await expect(() =>
+        chatItemService.postQuestion({
+          chatItemId: messageId,
+          userId,
+          topicId: 1,
+          content:
+            "親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事が",
+          quoteId: null,
+        }),
+      ).rejects.toThrowError()
+    })
   })
 
   describe("postAnswerのテスト", () => {
@@ -274,6 +302,20 @@ describe("ChatItemServiceのテスト", () => {
       expect(deliveredAnswer.isPinned).toBeFalsy()
       expect(deliveredAnswer.content).toBe("テストアンサー")
       expect((deliveredAnswer.quote as Question).id).toBe(target.id)
+    })
+
+    test("異常系_300文字を超えるanserを投稿", async () => {
+      const messageId = uuid()
+      await expect(() =>
+        chatItemService.postQuestion({
+          chatItemId: messageId,
+          userId,
+          topicId: 1,
+          content:
+            "親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事が",
+          quoteId: target.id,
+        }),
+      ).rejects.toThrowError()
     })
   })
 
